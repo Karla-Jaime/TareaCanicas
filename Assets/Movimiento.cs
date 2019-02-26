577f6cd5412d1f88a02f24dac41d8f1a7c412862
Karly-98 <Karly-98@DESKTOP-D2ALSSH>
2019-02-26 00:06:54 -0700

    mov

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.right * 400, ForceMode2D.Force);
+    }
+
+}
